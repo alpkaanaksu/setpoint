4f6169a61b524d6fc287bef77e5a0313f34f1887
Alp Kaan Aksu <hi@alpkaanaksu.com>
2024-03-19 20:58:22 +0100

    Continue working on the project

--- simulation/simulation.R
+++ simulation/simulation.R
@@ -1,13 +1,66 @@
 
 # A person is represented by a list:
-# list(sensitivity, adaptation_rate, SWB_set_point)
+# list(emotional_reactivity, adaptation_rate, SWB_set_point)
+
+# ====== Helpers ======
+
+library(dplyr)
+
+#' A generic logistic function
+#' 
+#' @param x 
+#' @param d
+#' @param a
+#' @param min
+#' @param max
+logistic <- function (x, d = 0, a = 1, min = 0, max = 1) {
+  min + (max - min)/(1 + exp(a * (d - x)))
+}
+
+#' Logistic function that delivers values between 0 - 1 for the interval 0 - 1
+#'
+#' @param x
+logistic_0_1 <- function(x) {
+  logistic(x, d = 0.5, a = 10)
+}
+
+#' Function representing a linear relationship.
+#' Overflows are prevented.
+#' 
+#' @param x
+#' @param min_y Minimum y value
+#' @param max_y Maximum y value
+#' @param min_x Minimum y value
+#' @param max_x Maximum y value
+#' 
+limited_linear <- function(
+    x,
+    min_y = 0, max_y = 1,
+    min_x = 0, max_x = 1
+  ) {
+  slope = (max_y - min_y) / (max_x - min_x)
+  
+  result <- case_when(
+    x > max_x ~ max_y,
+    x < min_x ~ min_y,
+    TRUE ~ slope * x
+  )
+}
 
 # ====== Relationships ======
 
-initial_reaction <- function (life_event, sensitivity) {
-    life_event * sensitivity
+#' Calculate initial emotional reaction of the person
+#' 
+#' @param life_event Objective valence of the life event
+#' @param  emotional_reactivity Tendency of the person to be influenced emotionally by external events
+initial_reaction <- function (life_event, emotional_reactivity) {
+    life_event * emotional_reactivity
 }
 
+#' Calculate the difference between the SWB and set point
+#' 
+#' @param SWB Current SWB
+#' @param SWB_set_point Baseline level of SWB for a person
 delta_SWB <- function (SWB, SWB_set_point) {
     SWB - SWB_set_point
 }
@@ -17,11 +70,11 @@ adaptation_rate <- function(delta_SWB_arg, adaptation_rate_person) {
 }
 
 SWB <- function (SWB, adaptation_rate) {
-  SWB + adaptation_rate
+  limited_linear(SWB + adaptation_rate)
 }
 
 event <- function(persons, life_event) {
-  persons[, SWB := initial_reaction(life_event, sensitivity) + SWB]
+  persons[, SWB := initial_reaction(life_event, emotional_reactivity) + SWB]
 }
 
 SWB_chain <- function(person) {
@@ -47,7 +100,7 @@ library("data.table")
 
 test_adaptation <- function (n, years, life_event) {
   data <- data.table(
-    sensitivity = runif(n, 0.5, 1),
+    emotional_reactivity = runif(n, 0.5, 1),
     adaptation_rate = runif(n, 0.4, 0.6),
     SWB_set_point = runif(n, 0, 1)
   )
@@ -71,12 +124,12 @@ test_adaptation <- function (n, years, life_event) {
 
 plot_single_person <- function (years, life_event) {
   person <- list(
-    sensitivity = runif(1, 0.5, 1),
+    emotional_reactivity = runif(1, 0.5, 1),
     adaptation_rate = runif(1, 0.4, 0.6),
     SWB_set_point = runif(1, 0, 1)
   )
   
-  person$SWB <- person$SWB_set_point + initial_reaction(life_event, person$sensitivity)
+  person$SWB <- person$SWB_set_point + initial_reaction(life_event, person$emotional_reactivity)
   
   person_data <- data.table() |> rbind(person)
   
@@ -94,8 +147,10 @@ plot_single_person <- function (years, life_event) {
     geom_point() +
     geom_line() +
     geom_hline(yintercept = person$SWB_set_point, color = "blue") +
+    geom_vline(xintercept = 0, color = "gray") +
     annotate("text", x = 0, y = person$SWB_set_point, label = "Personal Set Point", hjust = 0, vjust = 1.5, color = "blue")
 }
 
 test_adaptation(1000, 5, -0.2)
-plot_single_person(5, -0.2)
+plot_single_person(5, -1)
+
